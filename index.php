67048105e898a0513cb63f954f66a68c47021c4b
Victor Santiago <git.viktor@gmail.com>
2013-12-28 20:21:01 +0100

    Estructura inicial y TODO

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+  <!--[if !IE]-->
+<html lang="es" class="no-js">
+  <!--<![endif]-->
+  <!--[if IE 7]-->
+<html lang="es" class="ie7 no-js">
+  <!--<![endif]-->
+  <!--[if IE 8]-->
+<html lang="es" class="ie8 no-js">
+  <!--<![endif]-->
+  <head>
+    <meta charset = utf-8>
+    <title>XCom</title>
+    <link rel="stylesheet" type="text/css" href="vendor/css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="vendor/typos/fa/font-awesome.min.css">
+      <!--[if IE 7] -->
+    <link rel="stylesheet" type="text/css" href="vendor/typos/fa/font-awesome-ie7.min.css">
+      <!--<![endif] -->
+    <script type="text/javascript" src="vendor/js/modernizr.js"></script>
+  </head>
+<body>
+
+<div>XCom <i class="fa-anchor"></i> </div>
+
+  <script type="text/javascript" src="vendor/js/jquery.js"></script>
+  <script type="text/javascript" src="vendor/js/jquery-ui.js"></script>
+  <script type="text/javascript" src="vendor/js/underscore.js"></script>
+
+</body>
+</html>
